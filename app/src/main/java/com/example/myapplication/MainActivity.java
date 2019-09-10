512fb648002c94d600e1437c5a5f011adbf08e29
KnightShade <saurao2612@gmail.com>
2019-09-10 15:44:10 +0530

    register page design done

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailInput, passwordInput;
+    Button loginBtn;
+    ImageView registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Login");
+
+        emailInput = findViewById(R.id.login_email_input);
+        passwordInput = findViewById(R.id.login_password_input);
+        loginBtn = findViewById(R.id.login_btn);
+        registerBtn = findViewById(R.id.login_register_btn);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
