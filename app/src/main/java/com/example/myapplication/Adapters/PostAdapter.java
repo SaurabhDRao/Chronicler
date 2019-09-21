6d7214fd40969ef7bf5affd4fafa0ba50d6cc1b2
KnightShade <saurao2612@gmail.com>
2019-09-21 21:49:46 +0530

    users can like a post

--- app/src/main/java/com/example/myapplication/Adapters/PostAdapter.java
+++ app/src/main/java/com/example/myapplication/Adapters/PostAdapter.java
@@ -16,11 +16,16 @@ import android.widget.TextView;
 import com.example.myapplication.Models.Post;
 import com.example.myapplication.PostDetailsActivity;
 import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
@@ -45,8 +50,8 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
-        String picUrl = myData.get(position).getUserPhoto();
+    public void onBindViewHolder(@NonNull final PostViewHolder holder, final int position) {
+        final String picUrl = myData.get(position).getUserPhoto();
         if(!picUrl.equals("")) {
             Picasso.get().load(picUrl).into(holder.userImg);
         }
@@ -55,16 +60,73 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
         holder.usernameTv.setText(myData.get(position).getUsername());
         holder.dateTimeTv.setText(timeStampToString((long) myData.get(position).getPostedDateTime()));
 
-        int likeCount = myData.get(position).getLikeCount();
+        final int likeCount = myData.get(position).getLikeCount();
         holder.likeCountTv.setText(likeCount + "");
 
-        String likedUsers[] = myData.get(position).getLikedUsers().split(",");
+        final String likedUsers[] = myData.get(position).getLikedUsers().split(";");
         for(String likedUser : likedUsers) {
             if(likedUser.equals(currentUser.getUid())) {
                 holder.likeImgBtn.setImageDrawable(myContext.getResources().getDrawable(R.drawable.ic_thumb_up_blue_24dp));
                 break;
             }
         }
+
+        holder.likeImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                boolean flag = false;
+                for(String likedUser : likedUsers) {
+                    if(likedUser.equals(currentUser.getUid())) {
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if(!flag) {
+
+//                    holder.likeCountTv.setText((likeCount + 1) + "");
+                    final Post newPost = myData.get(position);
+
+                    String likedUsersStr = newPost.getLikedUsers();
+                    if(likedUsersStr.length() == 0)
+                        likedUsersStr = currentUser.getUid();
+                    else
+                        likedUsersStr += ";" + currentUser.getUid();
+                    newPost.setLikedUsers(likedUsersStr);
+                    newPost.setLikeCount(likeCount + 1);
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference databaseReference = database.getReference("Posts").child(newPost.getPostKey());
+
+                    HashMap<String, Object> postMap = new HashMap<>();
+                    postMap.put("title", newPost.getTitle());
+                    postMap.put("body", newPost.getBody());
+                    postMap.put("likeCount", newPost.getLikeCount());
+                    postMap.put("likedUsers", newPost.getLikedUsers());
+                    postMap.put("postKey", newPost.getPostKey());
+                    postMap.put("postedDateTime", newPost.getPostedDateTime());
+                    postMap.put("userId", newPost.getUserId());
+                    postMap.put("username", newPost.getUsername());
+                    postMap.put("userPhoto", newPost.getUserPhoto());
+
+                    databaseReference.updateChildren(postMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()) {
+                                myData.set(position, newPost);
+                                notifyItemChanged(position);
+
+                                holder.likeImgBtn.setImageDrawable(myContext.getResources().getDrawable(R.drawable.ic_thumb_up_blue_24dp));
+                            }
+                        }
+                    });
+
+
+                }
+
+            }
+        });
 
         Log.wtf("LIKED_USERS", myData.get(position).getLikedUsers() + " " + myData.get(position).getUserId());
     }
@@ -110,12 +172,14 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
                     i.putExtra("postKey", myData.get(pos).getPostKey());
                     i.putExtra("username", myData.get(pos).getUsername());
                     i.putExtra("userId", myData.get(pos).getUserId());
+                    i.putExtra("currentUserId", currentUser.getUid());
                     i.putExtra("likeCount", myData.get(pos).getLikeCount() + "");
                     i.putExtra("userImg", myData.get(pos).getUserPhoto());
                     i.putExtra("dateTime", timeStampToString((long) myData.get(pos).getPostedDateTime()));
+                    i.putExtra("dateTimeStamp", (long) myData.get(pos).getPostedDateTime());
 
                     boolean liked = false;
-                    String likedUsers[] = myData.get(pos).getLikedUsers().split(",");
+                    String likedUsers[] = myData.get(pos).getLikedUsers().split(";");
                     for(String likedUser : likedUsers) {
                         if(likedUser.equals(currentUser.getUid())) {
                             liked = true;
@@ -123,6 +187,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
                         }
                     }
                     i.putExtra("liked", liked);
+                    i.putExtra("likedUsers", myData.get(pos).getLikedUsers());
 
                     myContext.startActivity(i);
                 }
--- app/src/main/java/com/example/myapplication/PostDetailsActivity.java
+++ app/src/main/java/com/example/myapplication/PostDetailsActivity.java
@@ -1,13 +1,25 @@
 package com.example.myapplication;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
 
 public class PostDetailsActivity extends AppCompatActivity {
 
@@ -37,7 +49,7 @@ public class PostDetailsActivity extends AppCompatActivity {
             likeBtn.setImageDrawable(getResources().getDrawable(R.drawable.ic_thumb_up_blue_24dp));
         }
 
-        String picUrl = getIntent().getExtras().getString("userImg");
+        final String picUrl = getIntent().getExtras().getString("userImg");
         if(!picUrl.equals("")) {
             Picasso.get().load(picUrl).into(userImg);
         }
@@ -47,5 +59,48 @@ public class PostDetailsActivity extends AppCompatActivity {
         usernameTv.setText(getIntent().getExtras().getString("username"));
         dateTimeTv.setText(getIntent().getExtras().getString("dateTime"));
         likeCountTv.setText(getIntent().getExtras().getString("likeCount"));
+
+        likeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!liked) {
+                    String userId = getIntent().getExtras().getString("currentUserId");
+                    final int likeCount = Integer.parseInt(likeCountTv.getText().toString()) + 1;
+
+                    String likedUsersStr = getIntent().getExtras().getString("likedUsers");
+                    if(likedUsersStr.length() == 0)
+                        likedUsersStr = userId;
+                    else
+                        likedUsersStr += ";" + userId;
+
+                    String postKey = getIntent().getExtras().getString("postKey");
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference databaseReference = database.getReference("Posts").child(postKey);
+
+                    HashMap<String, Object> postMap = new HashMap<>();
+                    postMap.put("title", titleTv.getText().toString());
+                    postMap.put("body", bodyTv.getText().toString());
+                    postMap.put("likeCount", likeCount);
+                    postMap.put("likedUsers", likedUsersStr);
+                    postMap.put("postKey", postKey);
+                    postMap.put("postedDateTime", getIntent().getExtras().getLong("dateTimeStamp"));
+                    postMap.put("userId", getIntent().getExtras().getString("userId"));
+                    postMap.put("username", usernameTv.getText().toString());
+                    postMap.put("userPhoto", picUrl);
+
+                    databaseReference.updateChildren(postMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()) {
+                                likeCountTv.setText(likeCount + "");
+
+                                likeBtn.setImageDrawable(getResources().getDrawable(R.drawable.ic_thumb_up_blue_24dp));
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 }
